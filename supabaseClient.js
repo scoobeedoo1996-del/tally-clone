8080439025ec3f238261ca99e289d0679306a90c
scoobeedoo1996-del <scoobeedoo1996@gmail.com>
2026-03-29 07:32:34 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,8 @@
+;
 
 // supabaseClient.js
-const supabaseUrl = 'https://oibgebwvdmanddmkewnc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pYmdlYnd2ZG1hbmRkbWtld25jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MDQzOTUsImV4cCI6MjA5MDI4MDM5NX0.v3hYRDr7usrVaTPZDfaB3zKbaVko6cfirQ6P1CfKXtc';
+const sbUrl = 'https://oibgebwvdmanddmkewnc.supabase.co';
+const sbKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pYmdlYnd2ZG1hbmRkbWtld25jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MDQzOTUsImV4cCI6MjA5MDI4MDM5NX0.v3hYRDr7usrVaTPZDfaB3zKbaVko6cfirQ6P1CfKXtc';
 
-// Use 'window.' to ensure it's globally accessible across all your new modular files
-window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+// Use 'sbClient' instead of 'supabase' to avoid clashing with the library
+window.supabaseClient = supabase.createClient(sbUrl, sbKey);
